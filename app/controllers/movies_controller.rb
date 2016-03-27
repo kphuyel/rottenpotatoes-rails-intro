b9f16877d939b648002647b3afe00cb4be88b80b
kphuyel <kphuyel@uncc.edu>
2016-03-27 03:03:25 +0000

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,15 @@
 class MoviesController < ApplicationController
 
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-  end
+    def initialize
+        @all_ratings = Movie.all_ratings
+        super
+    end
 
-  def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
-  end
+    def show
+        id = params[:id] # retrieve movie ID from URI route
+        @movie = Movie.find(id) # look up movie by unique ID
+        # will render app/views/movies/show.<extension> by default
+    end
 
     def index
         redirect = false
@@ -46,32 +47,32 @@ class MoviesController < ApplicationController
         session[:ratings] = @ratings
     end
 
-  def new
-    # default: render 'new' template
-  end
+    def new
+        # default: render 'new' template
+    end
 
-  def create
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
-  end
+    def create
+        @movie = Movie.create!(params[:movie])
+        flash[:notice] = "#{@movie.title} was successfully created."
+        redirect_to movies_path
+    end
 
-  def edit
-    @movie = Movie.find params[:id]
-  end
+    def edit
+        @movie = Movie.find params[:id]
+    end
 
-  def update
-    @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
-  end
+    def update
+        @movie = Movie.find params[:id]
+        @movie.update_attributes!(params[:movie])
+        flash[:notice] = "#{@movie.title} was successfully updated."
+        redirect_to movie_path(@movie)
+    end
 
-  def destroy
-    @movie = Movie.find(params[:id])
-    @movie.destroy
-    flash[:notice] = "Movie '#{@movie.title}' deleted."
-    redirect_to movies_path
-  end
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        flash[:notice] = "Movie '#{@movie.title}' deleted."
+        redirect_to movies_path
+    end
 
 end
