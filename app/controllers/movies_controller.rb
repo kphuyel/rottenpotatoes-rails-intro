84d3b39f11900d1ea0a2802244611543ddc67e29
kphuyel <kphuyel@uncc.edu>
2016-03-27 03:13:26 +0000

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,52 +1,38 @@
 class MoviesController < ApplicationController
 
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  def index
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+
+    session[:ratings] = params[:ratings] if params[:ratings]
+    session[:sort]    = params[:sort]    if params[:sort]
+
+    if session[:ratings] || session[:sort]
+      case session[:sort]
+      when 'title'
+        @title_hilite = 'hilite'
+      when 'release_date'
+        @release_hilite = 'hilite'
+      end
+
+      session[:ratings] ||= @all_ratings
+      @ratings = session[:ratings]
+      @ratings = @ratings.keys if @ratings.respond_to?(:keys)
+      @movies = Movie.find(:all,
+                           order: session[:sort],
+                           conditions: ["rating IN (?)", @ratings])
+    else
+      @movies = Movie.all
+    end
+
+    if session[:ratings] != params[:ratings] || session[:sort] != params[:sort]
+      redirect_to movies_path(ratings: session[:ratings], sort: session[:sort])
+    end
   end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
+    @movie = Movie.find(id) # Look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
-  end
-
-  def index
-  
-    need_redirect = false
-    @all_ratings = Movie.get_all_ratings
-    ratings = params[:ratings] || session[:ratings] || {}
-    if ratings != {}
-      @ratings = ratings
-    else
-      @ratings = Hash.new
-      @all_ratings.each {|r| @ratings[r] = 1}
-    end
-    if params[:ratings]
-      session[:ratings] = @ratings
-    elsif session[:ratings]
-      need_redirect = true
-    end
-    
-    sort_by = params[:sort_by] || session[:sort_by]
-    if params[:sort_by]
-      session[:sort_by] = sort_by
-    elsif session[:sort_by]
-      need_redirect = true
-    end
-
-    if need_redirect
-      redirect_to :sort_by => sort_by, :ratings => @ratings
-    end
-
-    if sort_by == "title" 
-      @movies = Movie.where(rating: @ratings.keys).order("title ASC")
-      @title_class = "hilite"
-    elsif sort_by == "release_date"
-      @movies = Movie.where(rating: @ratings.keys).order("release_date ASC")
-      @release_date_class = "hilite"
-    else
-      @movies = Movie.where(rating: @ratings.keys)
-    end
   end
 
   def new
@@ -54,7 +40,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(movie_params)
+    @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -65,7 +51,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
+    @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
