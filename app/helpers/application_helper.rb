c35accac62d56630ad2091542792ee693058f2fd
kphuyel <kphuyel@uncc.edu>
2016-03-29 01:43:25 +0000

    hw 2 part 3 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def self.get_index_th_css_class(params)
+    css = {:title => 'th', :release_date => 'th'}
+    return css if params == {}
+    return css.merge({params[:sorting_by].to_sym => 'hilite'})
+  end
+  def self.get_selection(movie_ratings,params)
+    all_ratings = {}
+    selection = []
+    movie_ratings.each do | rating |
+      all_ratings.store(rating, true)
+      if params.has_key?(:ratings)
+        if params[:ratings].has_key?(rating)
+          all_ratings.store(rating, true)
+        else
+          all_ratings.store(rating, false)
+        end
+      end
+      selection << rating if all_ratings[rating]
+    end
+    return all_ratings, selection
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+  def self.get_ratings
+    ratings = []
+    Movie.select(:rating).distinct.each do | value|
+      ratings << value.rating
+    end
+    ratings
+  end
 end
